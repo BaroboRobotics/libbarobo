5c2bf8abee54c4759c903a86dbc0c74f9f6d2b30
David Ko <david.dk.ko@gmail.com>
2011-04-19 10:56:43 -0700

    Some updates

--- libbarobo/barobo.c
+++ libbarobo/barobo.c
@@ -1,11 +1,11 @@
 #include "barobo.h"
 
-int BR_pose(iMobot_t* iMobot, float angle[4], const char motorMask)
+int BR_pose(iMobot_t* iMobot, double angle[4], const char motorMask)
 {
   return 0;
 }
 
-int BR_move(iMobot_t* iMobot, float angle[4], const char motorMask)
+int BR_move(iMobot_t* iMobot, double angle[4], const char motorMask)
 {
   return 0;
 }
@@ -25,7 +25,7 @@ int BR_isBusy(iMobot_t* iMobot)
   return 0;
 }
 
-int BR_getJointAngles(iMobot_t* iMobot, float angle[4])
+int BR_getJointAngles(iMobot_t* iMobot, double angle[4])
 {
   return 0;
 }
--- libbarobo/barobo.h
+++ libbarobo/barobo.h
@@ -8,12 +8,18 @@ extern "C" {
 typedef struct iMobot_s {
 } iMobot_t;
 
-int BR_pose(iMobot_t* iMobot, float angle[4], const char motorMask);
-int BR_move(iMobot_t* iMobot, float angle[4], const char motorMask);
+int BR_move(iMobot_t* iMobot, double angle[4], const char motorMask);
+int BR_moveRelative(iMobot_t* iMobot, double angle[4], const char motorMask);
+int BR_moveJoint(iMobot_t* iMobot, double angle, int motorNumber);
+int BR_moveJointRelative(iMobot_t* iMobot, double angle, int motorNumber);
 int BR_stop(iMobot_t* iMobot);
 int BR_moveWait(iMobot_t* iMobot);
 int BR_isBusy(iMobot_t* iMobot);
-int BR_getJointAngles(iMobot_t* iMobot, float angle[4]);
+int BR_getJointAngles(iMobot_t* iMobot, double angle[4]);
+int BR_setSpeed(iMobot_t* iMobot, double speed[4], const char motorMask);
+int BR_getSpeed(iMobot_t* iMobot, double speed[4]);
+
+
 
 #ifdef __cplusplus
 }
