52654cc11434dce5169b29e888f1346e7ac7ec46
David Ko <david.dk.ko@gmail.com>
2011-07-07 09:20:52 -0700

    Added cpp demo for the iMobot library

---
+++ libimobot/Demos/simple_cpp/simple.cpp
@@ -0,0 +1,37 @@
+#include "imobot.h"
+
+int main()
+{
+  int i;
+  double enc;
+  CiMobot robot;
+
+  /* Set the robot to "home" position, where all joint angles are 0 degrees. */
+  for(i = 0; i < 4; i++) {
+    enc = 0;
+    robot.poseJoint(i, enc);
+  }
+  robot.moveWait();
+
+  /* Rotate each of the faceplates by 90 degrees */
+  robot.poseJoint(2, 90);
+  robot.poseJoint(3, 90);
+  /* Wait for the movement to complete */
+  robot.waitMotor(2);
+  robot.waitMotor(3);
+  /* Move the motors back to where they were */
+  robot.poseJoint(2, 0);
+  robot.poseJoint(3, 0);
+  robot.waitMotor(2);
+  robot.waitMotor(3);
+
+  /* The following 2 lines set up the robot to listen to Bluetooth remote
+   * control commands */
+  robot.initListenerBluetooth(20);
+  robot.listenerMainLoop();
+
+  /* Terminate control of the robot */
+  robot.terminate();
+
+  return 0;
+}
