f2329f0ed6a484087fccace997cfe8ee4b04f309
David Ko <david.dk.ko@gmail.com>
2011-07-21 00:28:45 +0100

    initBluetoothListener() now registers SDP service

--- libimobot/src/imobot.cpp
+++ libimobot/src/imobot.cpp
@@ -27,6 +27,8 @@ extern "C" {
 #include <sys/socket.h>
 #include <bluetooth/bluetooth.h>
 #include <bluetooth/rfcomm.h>
+#include <bluetooth/sdp.h>
+#include <bluetooth/sdp_lib.h>
 
 int BR_init(iMobot_t* iMobot)
 {
@@ -56,10 +58,76 @@ int BR_init(iMobot_t* iMobot)
   return 0;
 }
 
+sdp_session_t* register_service(uint8_t rfcomm_channel)
+{
+    //const char *service_uuid_string = "d1b58fd0-b3be-11e0-aff2-0800200c9a66";
+    uint32_t service_uuid_int[] = { 0xd1b58fd0, 0xb3be11e0, 0xaff20800, 0x200c9a66};
+    const char *service_name = "iMobot Control";
+    const char *service_dsc = "The iMobot Bluetooth control connection";
+    const char *service_prov = "iMobot";
+
+    uuid_t root_uuid, l2cap_uuid, rfcomm_uuid, svc_uuid;
+    sdp_list_t *l2cap_list = 0, 
+               *rfcomm_list = 0,
+               *root_list = 0,
+               *proto_list = 0, 
+               *access_proto_list = 0;
+    sdp_data_t *channel = 0, *psm = 0;
+
+    sdp_record_t *record = sdp_record_alloc();
+
+    // set the general service ID
+    sdp_uuid128_create( &svc_uuid, &service_uuid_int );
+    sdp_set_service_id( record, svc_uuid );
+
+    // make the service record publicly browsable
+    sdp_uuid16_create(&root_uuid, PUBLIC_BROWSE_GROUP);
+    root_list = sdp_list_append(0, &root_uuid);
+    sdp_set_browse_groups( record, root_list );
+
+    // set l2cap information
+    sdp_uuid16_create(&l2cap_uuid, L2CAP_UUID);
+    l2cap_list = sdp_list_append( 0, &l2cap_uuid );
+    proto_list = sdp_list_append( 0, l2cap_list );
+
+    // set rfcomm information
+    sdp_uuid16_create(&rfcomm_uuid, RFCOMM_UUID);
+    channel = sdp_data_alloc(SDP_UINT8, &rfcomm_channel);
+    rfcomm_list = sdp_list_append( 0, &rfcomm_uuid );
+    sdp_list_append( rfcomm_list, channel );
+    sdp_list_append( proto_list, rfcomm_list );
+
+    // attach protocol information to service record
+    access_proto_list = sdp_list_append( 0, proto_list );
+    sdp_set_access_protos( record, access_proto_list );
+
+    // set the name, provider, and description
+    sdp_set_info_attr(record, service_name, service_prov, service_dsc);
+    int err = 0;
+    sdp_session_t *session = 0;
+
+    // connect to the local SDP server, register the service record, and 
+    // disconnect
+    session = sdp_connect( BDADDR_ANY, BDADDR_LOCAL, SDP_RETRY_IF_BUSY );
+    err = sdp_record_register(session, record, 0);
+
+    // cleanup
+    sdp_data_free( channel );
+    sdp_list_free( l2cap_list, 0 );
+    sdp_list_free( rfcomm_list, 0 );
+    sdp_list_free( root_list, 0 );
+    sdp_list_free( access_proto_list, 0 );
+
+    return session;
+}
+
 int BR_initListenerBluetooth(iMobot_t* iMobot, int channel)
 {
   struct sockaddr_rc loc_addr = { 0 };
   int err;
+
+  // Register the service
+  register_service(channel);
 
   // allocate socket
   iMobot->socket = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
