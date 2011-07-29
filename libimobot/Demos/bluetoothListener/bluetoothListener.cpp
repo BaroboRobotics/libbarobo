545780fd53e6614b1f66cee8d00e7990a66fc021
David Ko <david.dk.ko@gmail.com>
2011-07-29 18:39:17 +0100

    Changed default speed for bluetooth listener

--- libimobot/Demos/bluetoothListener/bluetoothListener.cpp
+++ libimobot/Demos/bluetoothListener/bluetoothListener.cpp
@@ -3,7 +3,10 @@
 int main()
 {
   CiMobot robot;
-
+  int i;
+  for(i = 0; i < 4; i++) {
+    robot.setMotorSpeed(i, 50);
+  }
   robot.initListenerBluetooth(20);
   robot.listenerMainLoop();
   return 0;
