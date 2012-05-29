fe51a32e891f8c3674c40c69c61bee2e31d9f12d
David Ko <david.dk.ko@gmail.com>
2012-05-29 13:01:32 -0700

    Updated docs, swig header file code

--- libimobotcomms/PyMobot/test.py
+++ libimobotcomms/PyMobot/test.py
@@ -1,10 +1,13 @@
 from mobot import *
 
-robot = CMobot()
-robot.connect()
+robot1 = CMobot()
+robot2 = CMobot()
+robot1.connect()
+robot2.connect()
 angle1 = 0
 angle2 = 0
 angle3 = 0
 angle4 = 0
-robot.getJointAngles(angle1, angle2, angle3, angle4)
-print "The robot joint angles are %lf %lf %lf %lf" % angle1, angle2, angle3, angle4
+while 1:
+  ret,angle1,angle2,angle3,angle4 = robot1.getJointAngles()
+  robot2.moveToPIDNB(angle1, angle2, angle3, angle4)
