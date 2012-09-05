db6ae2f99584804390acc0161320e8dc6f396c79
David Ko <david.dk.ko@gmail.com>
2012-09-04 18:03:01 -0700

    Modifying some function names

--- libimobotcomms/mobot.cpp
+++ libimobotcomms/mobot.cpp
@@ -3167,7 +3167,7 @@ int CMobot::connectWithBluetoothAddress(const char* address, int channel)
   return Mobot_connectWithBluetoothAddress(_comms, address, channel);
 }
 
-int CMobot::connectWithIPAddress(const char* address, const char port[])
+int CMobot::connectWithIPAddress(const char* address, const char port[] = PORT)
 {
   return Mobot_connectWithIPAddress(_comms, address, port);
 }
