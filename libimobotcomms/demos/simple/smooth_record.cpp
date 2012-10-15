805f8d16bd72a620baa12b4e85d3a5269ed27f87
David Ko <david.dk.ko@gmail.com>
2012-10-15 14:14:14 -0700

    Added method to save/load smoothrecord data from file

--- libimobotcomms/demos/simple/smooth_record.cpp
+++ libimobotcomms/demos/simple/smooth_record.cpp
@@ -1,6 +1,7 @@
 /* Filename: simple.cpp
  * Rotate the faceplates by 90 degrees */
 #include <mobot.h>
+#include <stdlib.h>
 
 void playBack(CMobot* mobot,
     double* time,
@@ -10,6 +11,24 @@ void playBack(CMobot* mobot,
     double* angle4,
     int numPoints);
 
+void saveFile(double* time,
+    double* angle1,
+    double* angle2,
+    double* angle3,
+    double* angle4,
+    int num,
+    const char* filename);
+
+void loadFile(
+    double **time,
+    double **angle1,
+    double **angle2,
+    double **angle3,
+    double **angle4,
+    int *num,
+    const char *filename
+    );
+
 int main()
 {
   double *time;
@@ -18,6 +37,7 @@ int main()
   double *angle3;
   double *angle4;
   int numPoints;
+  int i;
   CMobot mobot;
 
   /* Connect to the paired MoBot */
@@ -44,6 +64,31 @@ int main()
       angle4,
       numPoints);
 
+  saveFile(time, 
+      angle1,
+      angle2,
+      angle3,
+      angle4,
+      numPoints,
+      "test.data");
+
+  loadFile(
+      &time,
+      &angle1,
+      &angle2,
+      &angle3,
+      &angle4,
+      &numPoints,
+      "test.data");
+  for(i = 0; i < numPoints; i++) {
+    printf("%lf, %lf, %lf, %lf, %lf\n",
+        time[i],
+        angle1[i],
+        angle2[i],
+        angle3[i],
+        angle4[i]);
+  }
+
   return 0;
 }
 
@@ -62,15 +107,71 @@ void playBack(CMobot* mobot,
       angle3[0],
       angle4[0]);
   for(i = 0; i < numPoints; i++) {
-    printf("%lf %lf %lf %lf\n", 
-        angle1[i],
-        angle2[i],
-        angle3[i],
-        angle4[i]);
     mobot->driveToNB(
         angle1[i],
         angle2[i],
         angle3[i],
         angle4[i]);
   }
+}
+
+void saveFile(double* time,
+    double* angle1,
+    double* angle2,
+    double* angle3,
+    double* angle4,
+    int num,
+    const char* filename)
+{
+  int i;
+  FILE *fp;
+  fp = fopen(filename, "w");
+  if(fp == NULL) {
+    fprintf(stderr,"Error opening file %s for writing\n", filename);
+    return;
+  }
+  fprintf(fp, "%d\n", num);
+  for(i = 0; i < num; i++) {
+    fprintf(fp, "%lf %lf %lf %lf %lf\n", time[i],
+        angle1[i],
+        angle2[i],
+        angle3[i],
+        angle4[i]
+        );
+  }
+  fclose(fp);
+}
+
+void loadFile(
+    double **time,
+    double **angle1,
+    double **angle2,
+    double **angle3,
+    double **angle4,
+    int *num,
+    const char *filename
+    )
+{
+  int i;
+  FILE *fp;
+  fp = fopen(filename, "r");
+  if(fp == NULL) {
+    fprintf(stderr,"Error opening file %s for reading\n", filename);
+    return;
+  }
+  fscanf(fp, "%d\n", num);
+  *time = (double*)malloc(sizeof(double) * (*num+1));
+  *angle1 = (double*)malloc(sizeof(double) * (*num+1));
+  *angle2 = (double*)malloc(sizeof(double) * (*num+1));
+  *angle3 = (double*)malloc(sizeof(double) * (*num+1));
+  *angle4 = (double*)malloc(sizeof(double) * (*num+1));
+  for(i = 0; i < *num; i++) {
+    fscanf(fp, "%lf %lf %lf %lf %lf\n", &(*time)[i],
+        &(*angle1)[i],
+        &(*angle2)[i],
+        &(*angle3)[i],
+        &(*angle4)[i]
+        );
+  }
+  fclose(fp);
 }
