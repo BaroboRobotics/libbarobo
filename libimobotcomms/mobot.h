a9a97627cb678d0b36852bc78ae20b08031b555d
David Ko <david.dk.ko@gmail.com>
2013-02-25 15:23:32 -0800

    Modified mobot.h to not use stdint.h for Windows

--- libimobotcomms/mobot.h
+++ libimobotcomms/mobot.h
@@ -44,7 +44,15 @@
 #endif
 
 #ifdef _WIN32
-#include <stdint.h>
+#ifndef _STDINT
+typedef signed char int8_t;
+typedef short int16_t;
+typedef int int32_t;
+
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
+#endif
 #include <ws2tcpip.h>
 #include <ws2bth.h>
 #endif
