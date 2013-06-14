95413f392431f5a286024b99f555a07e022bfc10
David Ko <david.dk.ko@gmail.com>
2013-06-14 14:36:42 -0700

    Changed exception communicating with robot to IOError

--- PyMobot/barobo/linkbot.py
+++ PyMobot/barobo/linkbot.py
@@ -26,12 +26,12 @@ class Linkbot():
     Use this function to control Linkbots that are currently 
     connected in BaroboLink. """
     if mobot.Mobot_connect(self._mobot) != 0:
-      raise RuntimeError("Error connecting to robot.")
+      raise IOError("Error connecting to robot.")
 
   def connectWithBluetoothAddress(self, address, channel=1):
     """Connect to a Bluetooth-enabled Linkbot"""
     if mobot.Mobot_connectWithBluetoothAddress(self._mobot, address, channel) != 0:
-      raise RuntimeError("Error connecting to robot.")
+      raise IOError("Error connecting to robot.")
 
   def disconnect(self):
     """Disconnect from a Linkbot"""
@@ -41,20 +41,20 @@ class Linkbot():
     """Return ADC reading from the breakout board."""
     rc, value = mobot.Mobot_getBreakoutADC(self._mobot, adc)
     if rc:
-      raise RuntimeError("Error communicating with robot. Return code {0}".format(rc))
+      raise IOError("Error communicating with robot. Return code {0}".format(rc))
     return value
   
   def getJointAngle(self, joint):
     rc, value = mobot.Mobot_getJointAngle(self._mobot, joint)
     if rc:
-      raise RuntimeError("Error communicating with robot. Return code {0}".format(rc))
+      raise IOError("Error communicating with robot. Return code {0}".format(rc))
     return rad2deg(value)
 
   def getJointAngles(self):
     """Return a list of joint angles in degrees"""
     r = mobot.Mobot_getJointAngles(self._mobot)
     if r[0] != 0:
-      raise RuntimeError("Error communicating with robot.")
+      raise IOError("Error communicating with robot.")
     else:
       return map(lambda x: rad2deg(x), r[1:4])
 
@@ -62,7 +62,7 @@ class Linkbot():
     """Returns a list: [millis, deg1, deg2, deg3]"""
     r = mobot.Mobot_getJointAnglesTime(self._mobot)
     if r[0] != 0:
-      raise RuntimeError("Error communicating with robot.")
+      raise IOError("Error communicating with robot.")
     else:
       ret = [r[1]]
       ret.extend( map(lambda x: rad2deg(x), r[2:5]) )
@@ -72,13 +72,13 @@ class Linkbot():
     """Drive joint 'joint' to 'angle' (degrees) using PID controller"""
     rc = mobot.Mobot_driveJointTo(self._mobot, joint, deg2rad(angle))
     if rc < 0:
-      raise RuntimeError("Error communicating with robot. Return code {0}".format(rc))
+      raise IOError("Error communicating with robot. Return code {0}".format(rc))
 
   def driveJointToNB(self, joint, angle):
     """Drive joint 'joint' to 'angle' (degrees) using PID controller"""
     rc = mobot.Mobot_driveJointToNB(self._mobot, joint, deg2rad(angle))
     if rc < 0:
-      raise RuntimeError("Error communicating with robot. Return code {0}".format(rc))
+      raise IOError("Error communicating with robot. Return code {0}".format(rc))
 
   def isMoving(self):
     """Return True if moving."""
@@ -88,25 +88,25 @@ class Linkbot():
     """Move a joint from its current location."""
     rc = mobot.Mobot_moveJoint(self._mobot, joint, deg2rad(angle))
     if rc < 0:
-      raise RuntimeError("Error communicating with robot. Return code {0}".format(rc))
+      raise IOError("Error communicating with robot. Return code {0}".format(rc))
 
   def moveJointNB(self, joint, angle):
     """Move a joint from its current location."""
     rc = mobot.Mobot_moveJointNB(self._mobot, joint, deg2rad(angle))
     if rc < 0:
-      raise RuntimeError("Error communicating with robot. Return code {0}".format(rc))
+      raise IOError("Error communicating with robot. Return code {0}".format(rc))
 
   def moveJointTo(self, joint, angle):
     """Move a joint to 'angle'."""
     rc = mobot.Mobot_moveJointTo(self._mobot, joint, deg2rad(angle))
     if rc < 0:
-      raise RuntimeError("Error communicating with robot. Return code {0}".format(rc))
+      raise IOError("Error communicating with robot. Return code {0}".format(rc))
 
   def moveJointToNB(self, joint, angle):
     """Move a joint to 'angle'."""
     rc = mobot.Mobot_moveJointToNB(self._mobot, joint, deg2rad(angle))
     if rc < 0:
-      raise RuntimeError("Error communicating with robot. Return code {0}".format(rc))
+      raise IOError("Error communicating with robot. Return code {0}".format(rc))
 
   def move(self, angle1, angle2, angle3):
     """Move the joints on a Linkbot
@@ -114,18 +114,14 @@ class Linkbot():
     Move joints from current location by amount specified in arguments in
     degrees. If the joint is not a movable joint (e.g. Joint 2 on a
     Linkbot-I), the argument is ignored."""
-    rc = self.moveNB(angle1, angle2, angle3)
-    if rc < 0:
-      raise RuntimeError("Error communicating with robot. Return code {0}".format(rc))
-    rc = self.moveWait()
-    if rc < 0:
-      raise RuntimeError("Error communicating with robot. Return code {0}".format(rc))
+    self.moveNB(angle1, angle2, angle3)
+    self.moveWait()
 
   def moveNB(self, angle1, angle2, angle3):
     """Nonblocking version ofthe move() function"""
     rc = mobot.Mobot_moveNB(self._mobot, deg2rad(angle1), deg2rad(angle2), deg2rad(angle3), 0)
     if rc < 0:
-      raise RuntimeError("Error communicating with robot. Return code {0}".format(rc))
+      raise IOError("Error communicating with robot. Return code {0}".format(rc))
 
   def moveTo(self, angle1, angle2, angle3):
     """Move the joints on a Linkbot
@@ -133,10 +129,10 @@ class Linkbot():
     Move the joints on a Linkbot to absolute positions."""
     rc = self.moveToNB(angle1, angle2, angle3)
     if rc < 0:
-      raise RuntimeError("Error communicating with robot. Return code {0}".format(rc))
+      raise IOError("Error communicating with robot. Return code {0}".format(rc))
     rc = self.moveWait()
     if rc < 0:
-      raise RuntimeError("Error communicating with robot. Return code {0}".format(rc))
+      raise IOError("Error communicating with robot. Return code {0}".format(rc))
 
   def moveToNB(self, angle1, angle2, angle3):
     """Nonblocking version of the moveTo() function"""
@@ -146,10 +142,11 @@ class Linkbot():
         deg2rad(angle3), 
         0)
     if rc < 0:
-      raise RuntimeError("Error communicating with robot. Return code {0}".format(rc))
+      raise IOError("Error communicating with robot. Return code {0}".format(rc))
 
   def moveWait(self):
     """Wait until a non-blocking movement function is finished moving"""
+    time.sleep(0.5)
     while mobot.Mobot_isMoving(self._mobot):
       time.sleep(0.5)
 
@@ -191,19 +188,19 @@ class Linkbot():
   def reset(self):
     rc = mobot.Mobot_reset(self._mobot)
     if rc < 0:
-      raise RuntimeError("Error communicating with robot. Return code {0}".format(rc))
+      raise IOError("Error communicating with robot. Return code {0}".format(rc))
 
   def resetToZero(self):
     """Reset the Linkbot to its zero positions."""
     rc = mobot.Mobot_resetToZero(self._mobot)
     if rc < 0:
-      raise RuntimeError("Error communicating with robot. Return code {0}".format(rc))
+      raise IOError("Error communicating with robot. Return code {0}".format(rc))
 
   def resetToZeroNB(self):
     """Reset the Linkbot to its zero positions."""
     rc = mobot.Mobot_resetToZeroNB(self._mobot)
     if rc < 0:
-      raise RuntimeError("Error communicating with robot. Return code {0}".format(rc))
+      raise IOError("Error communicating with robot. Return code {0}".format(rc))
 
   def setBuzzerFrequency(self, frequency):
     """Sets the buzzer to buz at a frequency specified in Hz.
@@ -211,7 +208,7 @@ class Linkbot():
     Set frequency to 0 to silence the buzzer"""
     rc = mobot.Mobot_setBuzzerFrequencyOn(self._mobot, int(frequency))
     if rc < 0:
-      raise RuntimeError("Error communicating with robot. Return code {0}".format(rc))
+      raise IOError("Error communicating with robot. Return code {0}".format(rc))
 
   def setColorRGB(self, r, g, b):
     """Set the multicolor LED on a linkbot.
@@ -220,13 +217,13 @@ class Linkbot():
     """
     rc = mobot.Mobot_setColorRGB(self._mobot, int(r), int(g), int(b))
     if rc < 0:
-      raise RuntimeError("Error communicating with robot. Return code {0}".format(rc))
+      raise IOError("Error communicating with robot. Return code {0}".format(rc))
 
   def setJointSpeed(self, joint, speed):
     """Set the constant-velocity speed of a joint to "speed", in deg/sec."""
     rc = mobot.Mobot_setJointSpeed(self._mobot, int(joint), float(deg2rad(speed)))
     if rc < 0:
-      raise RuntimeError("Error communicating with robot. Return code {0}".format(rc))
+      raise IOError("Error communicating with robot. Return code {0}".format(rc))
 
   def setMotorPower(self, joint, power):
     """Set the power of a joint.
@@ -235,7 +232,7 @@ class Linkbot():
     """
     rc = mobot.Mobot_setMotorPower(self._mobot, int(joint), int(power))
     if rc < 0:
-      raise RuntimeError("Error communicating with robot. Return code {0}".format(rc))
+      raise IOError("Error communicating with robot. Return code {0}".format(rc))
 
   def setMovementState(self, state1, state2, state3):
     """Set the movement state for the 3 joints of a Linkbot.
@@ -249,13 +246,13 @@ class Linkbot():
         state3,
         0)
     if rc < 0:
-      raise RuntimeError("Error communicating with robot. Return code {0}".format(rc))
+      raise IOError("Error communicating with robot. Return code {0}".format(rc))
    
   def stop(self):
     """Stop all motors."""
     rc = mobot.Mobot_stop(self._mobot) 
     if rc < 0:
-      raise RuntimeError("Error communicating with robot. Return code {0}".format(rc))
+      raise IOError("Error communicating with robot. Return code {0}".format(rc))
 
     
 class _LinkbotRecordThread(threading.Thread):
@@ -279,10 +276,17 @@ class _LinkbotRecordThread(threading.Thread):
         break
       self.runflag_lock.release()
       # Get the joint angles and stick them into our data struct
-      data = self.linkbot.getJointAnglesTime()
-      self.time.append(data[0])
-      self.angles[0].append(data[1])
-      self.angles[1].append(data[2])
-      self.angles[2].append(data[3])
-      time.sleep(self.delay)
+      try:
+        data = self.linkbot.getJointAnglesTime()
+        self.time.append(data[0])
+        self.angles[0].append(data[1])
+        self.angles[1].append(data[2])
+        self.angles[2].append(data[3])
+        time.sleep(self.delay)
+        numtries = 0
+      except IOError:
+        numtries += 1
+        if numtries >= 3:
+          raise
+        continue
     self.isRunning = False
