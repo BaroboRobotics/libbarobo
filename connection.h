ce72e6fc96a77ceec565a07ab01922ab5d94de7d
Harris Hancock <harris@barobo.com>
2013-07-23 16:44:01 -0700

    Start refactoring connection-related stuff out

---
+++ connection.h
@@ -0,0 +1,29 @@
+#ifndef _BAROBO_CONNECTION_H_
+#define _BAROBO_CONNECTION_H_
+
+typedef enum MOBOTconnectionType {
+  MOBOT_CONNECTION_TYPE_NONE,
+  MOBOT_CONNECTION_TYPE_TTY,
+  MOBOT_CONNECTION_TYPE_TCP,
+  MOBOT_CONNECTION_TYPE_BLUETOOTH,
+  MOBOT_CONNECTION_TYPE_ZIGBEE
+} MOBOTconnectionType;
+
+typedef enum MOBOTconnectionFraming {
+  MOBOT_CONNECTION_FRAMING_NONE,
+  MOBOT_CONNECTION_FRAMING_SFP
+} MOBOTconnectionFraming;
+
+typedef enum MOBOTconnectionStatus {
+  MOBOT_CONNECTION_STATUS_DOWN,
+  MOBOT_CONNECTION_STATUS_UP
+} MOBOTconnectionStatus;
+
+typedef struct MOBOTconnection MOBOTconnection;
+
+int Mobot_connectionInit (MOBOTconnection *conn);
+int Mobot_connectionConnect (MOBOTconnection *conn, );
+int Mobot_connectionDisconnect (MOBOTconnection *conn);
+int Mobot_serviceInput
+
+#endif
