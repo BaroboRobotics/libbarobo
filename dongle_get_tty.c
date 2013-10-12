ccb733a223912b927d65d144f8475580e887950a
Harris Hancock <harris@barobo.com>
2013-10-11 18:44:34 -0700

    add new USB product name

--- dongle_get_tty.c
+++ dongle_get_tty.c
@@ -9,7 +9,8 @@ typedef struct usb_dongle_id {
  * specific Mobot_dongleGetTTY() functions should depend on this data for
  * finding the dongle. Update this list as necessary. */
 static const usb_dongle_id g_barobo_usb_dongle_ids[] = {
-  { "Barobo, Inc.", "Mobot USB-Serial Adapter" }  // Linkbot (and Mobot?)
+  { "Barobo, Inc.", "Mobot USB-Serial Adapter" },
+  { "Barobo, Inc.", "Linkbot USB-Serial Adapter" }
 };
 
 /* For convenience. */
