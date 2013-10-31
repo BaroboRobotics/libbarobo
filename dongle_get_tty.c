c3c4475f4071cd07b139dea055a263b255fe77da
Harris Hancock <vortrab@gmail.com>
2013-10-30 22:09:38 -0700

    Make OS prefixes become suffixes on filename

--- dongle_get_tty.c
+++ dongle_get_tty.c
@@ -18,11 +18,11 @@ static const usb_dongle_id g_barobo_usb_dongle_ids[] = {
   (sizeof(g_barobo_usb_dongle_ids) / sizeof(g_barobo_usb_dongle_ids[0]))
 
 #ifdef _WIN32
-#include "win32_dongle_get_tty.c"
+#include "dongle_get_tty_win32.c"
 #elif defined(__linux__)
-#include "linux_dongle_get_tty.c"
+#include "dongle_get_tty_linux.c"
 #elif defined(__MACH__)
-#include "mach_dongle_get_tty.c"
+#include "dongle_get_tty_mach.c"
 #else
 #error No dongle_get_tty.c available for this platform.
 #endif
