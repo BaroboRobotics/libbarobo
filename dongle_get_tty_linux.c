1758ff262ee5dd760083103b83e368406558e29a
Harris Hancock <vortrab@gmail.com>
2013-11-15 16:26:03 -0800

    find dongle with sysfs instead of udev
    udev is a relatively heavyweight, arbitrary requirement to impose on a
    system.  Additionally, its marriage to systemd has made it controversial
    enough that I am not comfortable about its long-term adoption.
    Ironically, walking sysfs manually results in shorter, though arguably
    uglier, code.

--- dongle_get_tty_linux.c
+++ dongle_get_tty_linux.c
@@ -1,10 +1,79 @@
 #include <assert.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <errno.h>
 #include <ctype.h>
 #include <unistd.h>
 
+int Mobot_dongleGetTTY (char *buf, size_t len) {
+  const char* sysfs = getenv("SYSFS_PATH");
+  if (!sysfs) {
+    sysfs = "/sys";
+  }
+
+  size_t i;
+  int dongle_found = 0;
+  for (i = 0; !dongle_found && i < NUM_BAROBO_USB_DONGLE_IDS; ++i) {
+    char cmd[1024];
+
+    snprintf(cmd, sizeof(cmd), "find -O3 %s/devices -type f -name manufacturer -print0"
+      " | xargs -0 grep -lZ '^%s$'"
+      " | xargs -0 dirname -z"
+      " | xargs -0 -I{} find -O3 '{}' -maxdepth 1 -type f -name product -print0"
+      " | xargs -0 grep -lZ '^%s$'"
+      " | xargs -0 dirname -z"
+      " | xargs -0 -I{} find -O3 '{}' -type l -name subsystem -lname '*/tty' -print0"
+      " | xargs -0 dirname -z"
+      " | xargs -0 -I{} grep DEVNAME '{}'/uevent"
+      " | cut -d= -f2",
+      sysfs, g_barobo_usb_dongle_ids[i].manufacturer, g_barobo_usb_dongle_ids[i].product);
+
+    FILE* cmdstdout = popen(cmd, "r");
+
+    if (!cmdstdout) {
+      char errbuf[256];
+      strerror_r(errno, errbuf, sizeof(errbuf));
+      fprintf(stderr, "(barobo) ERROR: unable to execute find dongle command: %s", errbuf);
+      return -1;
+    }
+
+    char* line = NULL;
+    size_t n = 0;
+    ssize_t read;
+
+    while (-1 != (read = getline(&line, &n, cmdstdout))) {
+      line[read - 1] = '\0';  /* Overwrite newline */
+      snprintf(buf, len, "/dev/%s", line);
+      if (!access(buf, R_OK | W_OK)) {
+        printf("(barobo) INFO: dongle found at %s\n", buf);
+        dongle_found = 1;
+        break;
+      }
+      if (EACCES == errno) {
+        fprintf(stderr, "(barobo) WARNING: dongle found at %s, but user does not have "
+            "sufficient read/write permissions.\n", buf);
+      }
+      else {
+        char errbuf[256];
+        strerror_r(errno, errbuf, sizeof(errbuf));
+        fprintf(stderr, "(barobo) WARNING: attempted to access %s: %s\n", buf, errbuf);
+      }
+    }
+
+    free(line);
+    if (-1 == pclose(cmdstdout)) {
+      char errbuf[256];
+      strerror_r(errno, errbuf, sizeof(errbuf));
+      fprintf(stderr, "(barobo) WARNING: error closing find dongle command: %s\n", errbuf);
+      return -1;
+    }
+  }
+
+  return dongle_found ? 0 : -1;
+}
+
+#if 0
 static int util_replace_whitespace(const char *str, char *to, size_t len);
 static int util_replace_chars(char *str, const char *white);
 
@@ -265,3 +334,4 @@ static int util_replace_chars(char *str, const char *white)
         }
         return replaced;
 }
+#endif
