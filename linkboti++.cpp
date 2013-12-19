45232ada293274dde0e8f6f6d3bada6750892d0e
David Ko <david.dk.ko@gmail.com>
2013-12-19 12:19:38 -0800

    Added missing files

---
+++ linkboti++.cpp
@@ -0,0 +1,50 @@
+/*
+   Copyright 2013 Barobo, Inc.
+
+   This file is part of libbarobo.
+
+   BaroboLink is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   BaroboLink is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with BaroboLink.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <stdlib.h>
+#include "mobot.h"
+#include "mobot_internal.h"
+#include "linkbot.h"
+
+CLinkbotI::CLinkbotI()
+{
+}
+
+CLinkbotI::~CLinkbotI() 
+{
+}
+
+int CLinkbotI::connect()
+{
+  int rc = Mobot_connect(_comms);
+  if(rc) {
+    return rc;
+  }
+  if(_comms->formFactor != MOBOTFORM_I) {
+    fprintf(stderr, "Error: Connected robot is not a Linkbot-I.\n");
+    Mobot_disconnect(_comms);
+    return -1;
+  }
+}
+
+int CLinkbotI::connectWithSerialID(const char* serialID)
+{
+  return Mobot_connectWithSerialID(_comms, serialID);
+}
+
