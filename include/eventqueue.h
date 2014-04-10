82f7a0b5a6c9f56aa71a4d9acaa9f634dd236255
David Ko <david.dk.ko@gmail.com>
2014-04-10 14:03:06 -0700

    Fixed headers for Windows

--- include/eventqueue.h
+++ include/eventqueue.h
@@ -7,6 +7,7 @@
 #ifdef _MSC_VER
 typedef unsigned __int8 uint8_t;
 typedef unsigned __int16 uint16_t;
+typedef __int16 int16_t;
 typedef unsigned __int32 uint32_t;
 typedef unsigned __int64 uint64_t;
 #else
