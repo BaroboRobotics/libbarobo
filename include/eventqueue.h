dd6e1e3e54d29cb2b580c9d10c6843ef7e0a61f2
David Ko <david.dk.ko@gmail.com>
2014-04-10 13:55:39 -0700

    Changed eventqueue.h, increased moveWait() wait interval

--- include/eventqueue.h
+++ include/eventqueue.h
@@ -4,7 +4,14 @@
 #include <windows.h>
 #endif
 
+#ifdef _MSC_VER
+typedef unsigned __int8 uint8_t;
+typedef unsigned __int16 uint16_t;
+typedef unsigned __int32 uint32_t;
+typedef unsigned __int64 uint64_t;
+#else
 #include <stdint.h>
+#endif
 
 #include "thread_macros.h"
 
